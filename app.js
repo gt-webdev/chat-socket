3ee62195a38968d2e971c9dbbb227a2fb2f83cfd
Yuval Dekel <thedekel@gmail.com>
2013-02-26 06:01:29 -0500

    sockets are connecting, don't touch the tips

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ var app = express();
 app.set('view engine', 'ejs');
 
 
+app.get('/', function(req, res){
+  res.render('chat', {});
+});
+
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket){
+  socket.emit('test', {tool:'color?'});
+  socket.on('response', function(data){
+    console.log(data);
+  });
+});
 
 server.listen(8080);
