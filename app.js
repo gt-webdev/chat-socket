06b1deaa4c7afeac7da4ad37c34009ed2846a9e6
Yuval Dekel <thedekel@gmail.com>
2013-02-26 07:05:15 -0500

    memory

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
+var messages = [];
+
 app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res){
-  res.render('chat', {});
+  res.render('chat', {previous_msgs:messages});
 });
 
 var server = http.createServer(app);
@@ -16,6 +18,7 @@ io.sockets.on('connection', function(socket){
   socket.emit('test', {tool:'color?'});
 
   socket.on('msg', function(data){
+    messages.push({text:data.text, time:new Date().getTime(), user:data.user});
     io.sockets.emit('msg', {text:data.text, time:new Date().getTime(), user:data.user});
   });
 });
