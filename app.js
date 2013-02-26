6a82ff0328fe42a8189d72668e94c9bd0e7f417a
Yuval Dekel <thedekel@gmail.com>
2013-02-26 05:50:38 -0500

    we have socket.io

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+var http = require('http');
 var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
 
 
-app.listen(8080);
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
+
+server.listen(8080);
